e8432420718f38e706838eab5e5e6a8d52120461
Denys Fitsak <dfitsak@softjourn.com>
2016-11-17 12:25:20 +0200

    remove unused files

--- concept/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-// import form from './form';
-
-// combine all reducers
-const rootReducer = combineReducers({
-  // form,
-});
-
-export default rootReducer;
