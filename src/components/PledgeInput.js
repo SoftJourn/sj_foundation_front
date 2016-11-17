10b15142cc0ecf1905f0b32f8b1349abdad34d96
Denys Fitsak <dfitsak@softjourn.com>
2016-11-17 12:08:35 +0200

    change repo name

--- src/components/PledgeInput.js
+++ src/components/PledgeInput.js
@@ -6,13 +6,14 @@ import Modal from 'react-modal';
 import classNames from 'classnames';
 
 const customStyles = {
+  zIndex: 3,
   content : {
     top                   : '50%',
     left                  : '50%',
     right                 : 'auto',
     bottom                : 'auto',
     marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
+    transform             : 'translate(-50%, -50%)',
   }
 };
 
